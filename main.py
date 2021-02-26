a8c088c3d9ab15d0863ee148ec1f7238b2e2d62d
DjBee0312 <pashapasha0312@gmail.com>
2021-02-26 10:28:16 +0200

    lab1.2

--- main.py
+++ main.py
@@ -10,19 +10,45 @@ w = 0
 i = 0
 t = 0
 x = np.zeros(N)
+y = np.zeros(N)
 
-while i < n:
-    w += omega / n
-    i += 1
-    t = 0
-    while t < N:
+
+def fun1(i, n, w, omega, arr):
+    while i < n:
+        w += omega / n
+        i += 1
+        t = 0
         A = random.random()
         fi = np.random.uniform(-np.pi / 2, np.pi / 2)
-        x[t] += A * math.sin(w * t + fi)
-        t += 1
+        while t < N:
+            arr[t] += A * math.sin(w * t + fi)
+            t += 1
+
+
+fun1(0, n, w, omega, x)
+fun1(0, n + 20, w, omega, y)
 
 plt.plot(x)
 plt.ylabel('x(t)')
 plt.show()
-print(np.average(x))
-print(np.std(x) ** 2)
+
+plt.plot(y)
+plt.ylabel('y(t)')
+plt.show()
+
+print(np.average(x))  # Мат Ожидание
+print(np.std(x) ** 2)  # Дисперсия
+
+a = (x - np.mean(x)) / (np.std(x) * len(x))
+a2 = (x - np.mean(x)) / (np.std(x))
+b = (y - np.mean(y)) / (np.std(y))
+XXcor = np.correlate(a, a2, 'full')
+XYcor = np.correlate(a, b, 'full')
+
+plt.plot(XXcor)
+plt.ylabel('Автокореляция')
+plt.show()
+
+plt.plot(XYcor)
+plt.ylabel('Взаимная кореляция')
+plt.show()
