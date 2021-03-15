50aa10bf746bdfeea913d72debae443a2805dade
DjBee0312 <pashapasha0312@gmail.com>
2021-03-15 20:37:47 +0200

    lab2.1 task

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 import math
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 n = 14
 omega = 1800
-N = 256
+N = 4096  # 256
 w = 0
 i = 0
 t = 0
@@ -34,23 +36,36 @@ plt.show()
 p = 0
 k = 0
 
-while p < N:
-    k = 0
-    while k < N:
-        f[p] += x[k] * (np.cos(2 * np.pi * p * k / N) - np.sin(2 * np.pi * p * k / N) * 1j)
-        k += 1
-    p += 1
+start_time = time.time()
+iterator = 0
+while iterator < 10:
+    while p < N:
+        k = 0
+        while k < N:
+            f[p] += x[k] * (np.cos(2 * np.pi * p * k / N) - np.sin(2 * np.pi * p * k / N) * 1j)
+            k += 1
+        p += 1
+    iterator += 1
 
-print(f)
+end_time = time.time()
+average_time = (end_time - start_time) / 10
+print("start_time")
+print(start_time)
+print("end_time")
+print(end_time)
+print("global_time")
+print(end_time - start_time)
+print("average_time")
+print(average_time)
+
 A = np.zeros(N)
-
 A = np.abs(f)
 A = 2 * A / N
 print(A)
 
-H = list(range(1, 128))
+H = list(range(1, 2048))  # 128
 
 plt.plot(A)
 plt.ylabel("Фурье")
-plt.xlim(0, 128)
+plt.xlim(0, 2048)  # 128
 plt.show()
